c3a4173574582addae6d12053d0638e7a2c78d6c
chocoding <galmeagi2@naver.com>
2025-05-12 17:57:59 +0900

    C:/Program Files/Git/reissue 요청 시 기본 axios 사용

--- js/axiosInstance.js
+++ js/axiosInstance.js
@@ -36,7 +36,7 @@ axiosInstance.interceptors.response.use(
                 const refreshToken = localStorage.getItem('refreshToken');
 
                 // /reissue 요청해서 새로운 accessToken 발급
-                const reissueResponse = await axiosInstance.post('/reissue', {}, {headers: {'Authorization-Refresh': refreshToken}});
+                const reissueResponse = await axios.post('/reissue', {}, {headers: {'Authorization-Refresh': refreshToken}});
 
                 const newAccessToken = reissueResponse.headers['authorization'].replace("Bearer ", "");
                 localStorage.setItem('accessToken', newAccessToken);
